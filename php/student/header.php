c69df94a905d252e7d9f6f0613509b47f5b07043
Denver Salazar <denversalazar20@gmail.com>
2024-10-20 08:43:11 +0800

    Innitial Commit

---
+++ php/student/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400..900&display=swap" rel="stylesheet">
+</head>
+
+<style>
+    body{
+        font-family: "Cinzel", serif;
+        font-optical-sizing: auto;
+        font-style: normal;
+        background-color: #898989;
+    }
+</style>
+
+<body>
+    <section style='text-align: center; color: white; background: rgba(255, 251, 251, 0.1); padding: 10px 20px;'>
+        <h1 >LAB EXERCISE 2</h1>
+        <h1>USING PHP Array, Loops and Bootstrap 2</h1>
+    </section>
+</body>
+</html>
